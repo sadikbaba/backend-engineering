a178df6ad5c4f79f3b164e54e8fb50c3ad14d4f8
Sadikbaba <sadikbaba360@gmail.com>
2026-09-26 09:36:52 +0100

    corret model name

--- phase-01-api-engineering/backend/notes/models.py
+++ phase-01-api-engineering/backend/notes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-class NotesModel(models.Model):
+class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
